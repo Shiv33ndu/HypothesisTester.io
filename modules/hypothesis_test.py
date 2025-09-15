9d0772dcc27fe1eb4835ea056d7d0cc0a7165704
Shivendu Kumar <shiv.end.you@gmail.com>
2025-09-15 21:31:57 +0530

    Minor param changes

--- modules/hypothesis_test.py
+++ modules/hypothesis_test.py
@@ -26,7 +26,7 @@ def dispatch_test(llm_output_json: Dict[str, Any], df: pd.DataFrame):
             test_results = data_prep.run_two_sample_ttest(prepared_data, params) if test_name == "Two-Sample Independent t-test" else data_prep.run_mann_whitney_u(prepared_data, params)
         elif test_name in ["ANOVA", "Kruskal-Wallis H Test"]:
             prepared_data = data_prep.prepare_data_for_anova(df, params)
-            test_results = data_prep.run_anova(prepared_data, params) if test_name == "ANOVA" else data_prep.run_kruskal(prepared_data, params)
+            test_results = data_prep.run_anova(prepared_data) if test_name == "ANOVA" else data_prep.run_kruskal(prepared_data, params)
         elif test_name in ["Chi-Square Test of Independence", "Fisher's Exact Test"]:
             prepared_data = data_prep.prepare_data_for_chi_square(df, llm_output_json)
             test_results = data_prep.run_chi_square_independence(prepared_data) if test_name == "Chi-Square Test of Independence" else data_prep.run_fishers_exact(prepared_data)
@@ -61,5 +61,5 @@ def dispatch_test(llm_output_json: Dict[str, Any], df: pd.DataFrame):
 
     except Exception as e:
         error_msg = {"error": str(e)}
-        return {'result': 'Empty Values from exception'}, error_msg
+        return error_msg, {'result': 'Empty Values from exception'}
 
